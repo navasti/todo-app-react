d7bde3746aa8150e8d42b61659d63a012ea1b490
Paweł Szostak <szostak_pawel@outlook.com>
2020-01-09 19:35:33 +0100

    Added initial structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      data: null,
+      tasks: [
+        {"id": 1, "description": "Clean up the apartment"},
+        {"id": 2, "description": "Do shopping"},
+        {"id": 3, "description": "Take the dog for a walk"}
+      ],
+    }
+  }
+  componentDidMount(){
+    console.log(this.state.tasks)
+  }
+  render(){
+    const {tasks} = this.state;
+    return(
+      <div id="container">
+        <div className="add-task">
+          <h1>Got a new task to do?</h1>
+          <input type="text"/>
+          <div className="important">
+            <h2>Is it important?</h2>
+            <span className="yes">Yes</span>
+            <span className="no">No</span>
+          </div>
+          <button className="addBtn">Add task</button>
+        </div>
+        <div className="search">
+          <input type="text" placeholder="Search for certain task"/>
+        </div>
+        <div className="tasks">
+          <div className="undone">
+            <h3>Remaining tasks</h3>
+            <ul>
+              <li>{tasks[0].description}</li>
+              <li>{tasks[1].description}</li>
+            </ul>
+            </div>
+          <div className="done">
+          <h3>Accomplished tasks</h3>
+            <ul>
+              <li>{tasks[2].description}</li>
+              <div className="clearBtn">Clear</div>
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
