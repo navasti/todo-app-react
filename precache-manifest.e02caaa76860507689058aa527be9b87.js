08f035e537285e1e47988625550ab092de25e087
Paweł Szostak <szostak_pawel@outlook.com>
2020-01-10 16:09:16 +0100

    Updates

---
+++ precache-manifest.e02caaa76860507689058aa527be9b87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930477afa948de97b12eb52d51e3a486",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "b65f2ee1c472984f057e",
+    "url": "/todo-react/static/css/main.b6bc5c63.chunk.css"
+  },
+  {
+    "revision": "408277a90379f24d6670",
+    "url": "/todo-react/static/js/2.e7504007.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-react/static/js/2.e7504007.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b65f2ee1c472984f057e",
+    "url": "/todo-react/static/js/main.c596bedb.chunk.js"
+  },
+  {
+    "revision": "6b92df699a98dfc512dc",
+    "url": "/todo-react/static/js/runtime-main.9ac7cc46.js"
+  }
+]);
